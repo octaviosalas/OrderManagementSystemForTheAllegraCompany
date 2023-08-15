1051db9228d8ed37ad7406e2da7e31d8e034dd33
Franco Jara <franco@easytechgreen.com>
2023-08-15 20:21:53 -0300

    Add env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		PS_API_URL: process.env.PS_API_URL,
+		PS_API_TOKEN: process.env.PS_API_TOKEN,
+		API_URL: process.env.API_URL,
+		API_TOKEN: process.env.API_TOKEN,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
