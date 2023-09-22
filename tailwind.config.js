5812c7f887c1284b6696fdd3afd24cb276e35751
Franco Jara <franco@easytechgreen.com>
2023-09-22 19:06:12 -0300

    Add screens for media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,16 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        "2xs": "1px",
+        "xxxs": "280px",
+        "xxs": "480px",
+        "sm": "640px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1280px",
+        "2xl": "1536px",
+      },
     },
   },
   plugins: [
