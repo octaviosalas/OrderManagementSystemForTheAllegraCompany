4dfdeb6e223ceb8cec27c7640512644c486e5db7
Octavio Salas <salasoctavio129@gmail.com>
2023-09-30 10:18:21 -0300

    myChamges

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) { 
 
-	console.log(window.location.pathname)
+
 	return (
 		<html lang="en" className="white">
 			<body className={inter.className}>
--- app/page.js
+++ app/page.js
@@ -7,9 +7,6 @@ import { useRouter } from 'next/navigation'
 import { useState } from "react";
 
 
-
-
-
 export default function Home() {
 
  
